8a51cb5ebcd811ca4d4fa1e5b8d189cc1346798b
nikhil yadav <nikhlyadav50586@gmail.com>
2022-09-18 14:10:20 +0530

    updating mongo_url connection string

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+app.use(cookieParser())
 
 // app.use((req, res, next) => {
 //     //req.requestTime = new Date().toISOString();
